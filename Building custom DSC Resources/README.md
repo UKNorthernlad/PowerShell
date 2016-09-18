a70158f82d17df5f54d9fa0541a6abfe8ca7c360
HouseAndProud <brianking@theinternet>
2016-09-18 03:29:14 +0100

    Added DSC Resources example

---
+++ Building custom DSC Resources/README.md
@@ -0,0 +1,7 @@
+# Build Custom Windows PowerShell Desired State Configuration Resources
+
+There are [5 different ways to create a custom PowerShell DSC resource][https://msdn.microsoft.com/en-us/powershell/dsc/authoringResource].
+
+These are my examples showing how its done.
+
+- **ResourceDesignerExample.ps1**. Using the [Resource Designer Tool][https://msdn.microsoft.com/en-us/powershell/dsc/authoringresourcemofdesigner] in the xDscResourceDesigner module.
---
+++ Building custom DSC Resources/ResourceDesignerExample.ps1
@@ -0,0 +1,11 @@
+﻿# https://msdn.microsoft.com/en-us/powershell/dsc/authoringResourceMofDesigner
+
+# Start off my defining the schema for the resource....
+$UserName = New-xDscResourceProperty –Name UserName -Type String -Attribute Key
+$Ensure = New-xDscResourceProperty –Name Ensure -Type String -Attribute Write –ValidateSet "Present", "Absent"
+$DomainCredential = New-xDscResourceProperty –Name DomainCredential -Type PSCredential -Attribute Write
+$Password = New-xDscResourceProperty –Name Password -Type PSCredential -Attribute Write
+
+
+# Now create the resource with the requried properties in a named location.
+New-xDscResource –Name Demo_ADUser –Property $UserName, $Ensure, $DomainCredential, $Password –Path 'C:\Program Files\WindowsPowerShell\Modules' –ModuleName Demo_DSCModule
