43649f723f22f9b4f98d4857ca155d009b0ea35d
Billy Bunter <billy@bunter.com>
2022-02-22 22:02:45 +0000

    Build definitions and start pipeline

---
+++ AzureDevOps/Get Build Definitions.ps1
@@ -0,0 +1,48 @@
+﻿# Only change the following  values:
+$PAT = "gdfsgdfsge5436ygdgrthyt67rhfyht"
+$invokeURL = "https://dev.azure.com/myorg/demos" + "/_apis/build/definitions?api-version=6.0"
+
+# Don't change anything below here:
+
+function Build-Credential
+{
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$true)]
+        [string]$PATToken
+    )
+
+    # Build the Basic Header String (we don't need a username
+    $clearAuthHeader = ":" + $PAT
+
+    # Get the raw bytes of the clearAuthHeader
+    $clearAuthHeaderBytes = [System.Text.Encoding]::UTF8.GetBytes($clearAuthHeader)
+
+    # Base64 Encode the raw bytes 
+    return [System.Convert]::ToBase64String($clearAuthHeaderBytes)
+}
+
+
+# Create a credential object
+$Credential = Build-Credential -PATToken $PAT
+
+$headers = @{
+ "Content-Type"="application/json"
+ "Authorization"= "Basic $Credential"
+ }
+
+# Invoke the request.
+# REST API Reference = https://docs.microsoft.com/en-us/rest/api/azure/devops/build/definitions/list?view=azure-devops-rest-6.0
+$buildDefinitions = Invoke-RestMethod -Method Get -Uri $invokeURL -Headers $headers
+Write-Host "`nThe following Build Definitions are available:`n"
+
+foreach ($buildDefinition in $buildDefinitions.value)
+{
+    Write-Host "Build Defintion ID = "   $($buildDefinition.id)
+    Write-Host "Build Defintion name = " $($buildDefinition.name)
+    Write-Host "Build Defintion uri = "  $($buildDefinition.uri)
+    Write-Host "Build Defintion url = "  $($buildDefinition.url)
+
+    Write-Host  
+}
---
+++ AzureDevOps/StartPipeline.ps1
@@ -0,0 +1,49 @@
+﻿# Only change the following  values:
+$PAT = "gdfsgdfsge5436ygdgrthyt67rhfyht"
+$invokeURL = "https://dev.azure.com/myorg/demos" + "/_apis/pipelines/61/runs?api-version=6.1-preview.1"
+
+function Build-Credential
+{
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$true)]
+        [string]$PATToken
+    )
+
+    # Build the Basic Header String (we don't need a username
+    $clearAuthHeader = ":" + $PAT
+
+    # Get the raw bytes of the clearAuthHeader
+    $clearAuthHeaderBytes = [System.Text.Encoding]::UTF8.GetBytes($clearAuthHeader)
+
+    # Base64 Encode the raw bytes 
+    return [System.Convert]::ToBase64String($clearAuthHeaderBytes)
+}
+
+
+# Create a credential object
+$Credential = Build-Credential -PATToken $PAT
+
+$headers = @{
+ "Content-Type"="application/json"
+ "Authorization"= "Basic $Credential"
+ }
+
+# Invoke a new build
+# REST API Reference = https://docs.microsoft.com/en-us/rest/api/azure/devops/build/builds/queue?view=azure-devops-rest-6.0
+# Start a new Build a given Build Defintion ID from the previous call - harded to 61 in this example
+
+# Create the HTTP Post body which contains any parameters or variables you want to pass,
+$body = '
+{
+    "templateParameters": {
+        "enableImportantThing": "True",
+        "targetHostPool": "Pool3",
+        "zoneNumber": "Z3"
+    }
+}
+'
+
+# Invoke the request.
+Invoke-RestMethod -Method POST -Uri $invokeURL  -Body $body -Headers $headers
